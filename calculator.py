6f934b69106193214037a02c690a5330e10a2b9e
annaanx24 <annaxia24@gmail.com>
2020-07-13 13:49:25 +0000

    updated covid calc

--- calculator.py
+++ calculator.py
@@ -5,8 +5,8 @@ import definitions
 # print("Welcome to the Python Calculator\n\nWhat would you like to do?")
 # choice = input()
 
-# print (definitions.getTotalPosCases())
-# print (definitions.getDeath())
-# print (definitions.getHosp())
+print (definitions.getTotalPosCases())
+print (definitions.getDeath())
+print (definitions.getHosp())
 
 print (definitions.getResources())
